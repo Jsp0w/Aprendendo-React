e760447872f931465f5d06aa260a9cec959b536d
Jsp0w <joaopauloleireslopes.15@gmail.com>
2026-02-06 10:11:46 -0300

    Updates

---
+++ assets/useEffect-DwnkbKda.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BARqkNQM.js";function t(){return o.jsx("div",{children:'Hello "/useEffect"!'})}export{t as component};
---
+++ assets/useQuery-Bm08Hwmp.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BARqkNQM.js";function n(){return o.jsx("div",{children:'Hello "/useQuery"!'})}export{n as component};
