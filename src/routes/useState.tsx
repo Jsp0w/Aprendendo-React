1f0adcd0456e56f3e50c450bd0e562261ff51424
Jsp0w <joaopauloleireslopes.15@gmail.com>
2026-02-24 11:50:30 -0300

    implementação dos parenteses na calculadora

--- src/routes/useState.tsx
+++ src/routes/useState.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { ReactNode, useState, Component, Children } from 'react'
-import Header from '../components/Header'
+import { ReactNode, useState, Component, Children, useEffect } from 'react'
 
 export const Route = createFileRoute('/useState')({
   component: RouteComponent,
@@ -13,14 +12,14 @@ function RouteComponent() {
   //   if(!isNaN(parseInt(event.key))){
   //     setCalcScreen(addCharOnCalc(CalcScreen,event.key))
   //   }
-  // })
+  // }) 
 
   return (
     <>
       <div className='grid grid-cols-4 grid-rows-6 items-center justify-center h-[calc(100dvh-64px)] w-[calc(97px*4+12px*3)] text-3xl gap-3 py-4 m-auto font-bold'>
         <input className='h-full col-start-1 col-end-5 text-end' onChange={(element)=>{setCalcScreen(element.target.value)}} value={CalcScreen}></input>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-red-600 hover:text-red-500 active:bg-red-600 active:text-white col-start-1 row-start-2 ' onClick={()=>{setCalcScreen('0')}}>AC</button>
-        <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-red-600 hover:text-red-500 active:bg-red-600 active:text-white col-start-2 row-start-2 ' onClick={()=>{setCalcScreen(CalcScreen.substring(0,CalcScreen.length-1))}}>C</button>
+        {/* <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-red-600 hover:text-red-500 active:bg-red-600 active:text-white col-start-2 row-start-2 ' onClick={()=>{setCalcScreen(CalcScreen.substring(0,CalcScreen.length-1))}}>C</button> */}
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-emerald-700 hover:text-emerald-600 active:bg-emerald-700 active:text-white col-start-1 row-start-5' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'1'))}}>1</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-emerald-700 hover:text-emerald-600 active:bg-emerald-700 active:text-white col-start-2 row-start-5' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'2'))}}>2</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-emerald-700 hover:text-emerald-600 active:bg-emerald-700 active:text-white col-start-3 row-start-5' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'3'))}}>3</button>
@@ -30,50 +29,101 @@ function RouteComponent() {
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-emerald-700 hover:text-emerald-600 active:bg-emerald-700 active:text-white col-start-1 row-start-3' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'7'))}}>7</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-emerald-700 hover:text-emerald-600 active:bg-emerald-700 active:text-white col-start-2 row-start-3' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'8'))}}>8</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-emerald-700 hover:text-emerald-600 active:bg-emerald-700 active:text-white col-start-3 row-start-3' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'9'))}}>9</button>
-        <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-emerald-700 hover:text-emerald-600 active:bg-emerald-700 active:text-white col-start-1 row-start-6' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'0'))}}>0</button>
-        {/* <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-2 row-start-2' disabled={true} onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'()'))}}>( )</button> */}
+        <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-emerald-700 hover:text-emerald-600 active:bg-emerald-700 active:text-white col-start-1 col-end-3 row-start-6' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'0'))}}>0</button>
+        <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-2 row-start-2' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'()'))}}>( )</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-3 row-start-2' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'%'))}}>%</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-4 row-start-2' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'÷'))}}>÷</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-4 row-start-3' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'*'))}}>*</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-4 row-start-4' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'-'))}}>-</button>
         <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-4 row-start-5' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'+'))}}>+</button>
-        {/* <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-1 row-start-6' disabled={true} onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,'+/-'))}}>+/-</button> */}
-        <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-2 row-start-6' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,','))}}>,</button>
-        <button className='rounded-3xl h-full bg-cyan-600 text-white hover:bg-cyan-700 hover:cursor-pointer active:bg-cyan-800 col-start-3 col-end-5 row-start-6' onClick={()=>{setCalcScreen(calcular(CalcScreen))}}>=</button>
+        <button className='rounded-3xl h-full bg-stone-500/10 hover: cursor-pointer text-cyan-600 hover:text-cyan-500 active:bg-cyan-600 active:text-white col-start-3 row-start-6' onClick={()=>{setCalcScreen(addCharOnCalc(CalcScreen,','))}}>,</button>
+        <button className='rounded-3xl h-full bg-cyan-600 text-white hover:bg-cyan-700 hover:cursor-pointer active:bg-cyan-800 col-start-4 row-start-6' onClick={()=>{setCalcScreen(calcular(CalcScreen))}}>=</button>
       </div>
     </>
   )
 }
 
 function addCharOnCalc(count:string, numberToAdd:string):string{
+    console.clear()
+  
   if(count == '0' || count == 'NaN' ){
     if(!isNaN(parseInt(numberToAdd))){
       return numberToAdd
-    }
+    }    
     
+    if (numberToAdd == '()'){
+      return '('
+    }
     
     if (numberToAdd == ','){
       return '0,'
     }
-
+    
+    if (numberToAdd == '+/-'){
+      return '(-'
+    }
+    
     return '0'
   }
-
+  
+  const lestChar = count[count.length - 1]
+   
   if(numberToAdd == ','){
-    if(count[count.length-1] == '%' && count[count.length-1] != ','){
+    
+    if(lestChar == '%'){
       return count+'*0,'
     }
-
-    if(isNaN(parseInt(count[count.length-1])) && count[count.length-1] != ','){
+    
+    if(isNaN(parseInt(lestChar)) && lestChar != ','){
       return count+'0,'
     }
-
-    if(isNaN(Number(count.substring(count.length-2, count.length)))){
+    
+    if(isNaN(Number(lestChar))){
       return count
+    }
+
+    let teset = count
+    const mapOp = ['+','-','*','÷']
+    for (let i = 0; i < 4; i++) {
+      
     }
   }
   
-  if(count[count.length - 1] == '%'){
+  // if(numberToAdd == '+/-'){
+  //   console.group('mais ou menos')
+  //   console.log(lestChar);
+  //   console.groupEnd()
+    
+  //   if(isNaN(Number(lestChar)) && lestChar != ','){
+      
+  //     if(lestChar == ')' || lestChar == '%'){
+  //       return count+'*(-'
+  //     }
+
+  //     return count+'(-'
+  //   }
+
+  //   return count
+  // }
+  
+  if(numberToAdd == '()'){
+
+    if(lestChar == '('){
+      return count+'('
+    }
+    
+    
+    if (count.split('(').length == count.split(')').length) {
+      if(lestChar == ')' || lestChar == '%' || lestChar == ',' || !isNaN(Number(lestChar))){
+        return count+'*('
+      }
+      return count+'('
+    }
+
+    return count+')'
+  }
+
+  if(lestChar == '%'){
 
     if (numberToAdd == '%') {
       return count
@@ -89,12 +139,12 @@ function addCharOnCalc(count:string, numberToAdd:string):string{
   }
 
   if (numberToAdd == '%'){
-    if(isNaN(Number(count[count.length-1]))){
+    if(isNaN(Number(lestChar))){
       return count
     }
   }
 
-  if(numberToAdd != '%' && isNaN(Number(numberToAdd)) && isNaN(Number(count[count.length -1]))){
+  if(numberToAdd != '%' && lestChar != '(' && lestChar != ')' && isNaN(Number(numberToAdd)) && isNaN(Number(lestChar))){
     return count.substring(0,count.length-1) + numberToAdd
   }
 
@@ -104,8 +154,24 @@ function addCharOnCalc(count:string, numberToAdd:string):string{
 function calcular(conta:string): string {
 
   try{
-    return String(eval(conta.replaceAll('÷','/').replaceAll(',','.').replaceAll('%','/100'))).replaceAll('.',',')
+    
+    return String(
+      eval(
+        conta.split('(').length == conta.split(')').length?
+
+        conta.replaceAll('÷','/').replaceAll(',','.').replaceAll('%','/100'):
+
+        (
+          conta+')'.repeat(
+            (conta.split('(').length - conta.split(')').length)
+          )
+        ).replaceAll('÷','/').replaceAll(',','.').replaceAll('%','/100')
+
+      )).replaceAll('.',',')
+
   }catch(error){
-    return 'error'
+    
+    return 'NaN'
+  
   }
 }
