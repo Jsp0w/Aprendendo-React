93273ecc8bf7bc829b7cbb6717de39f00fd8f699
Jsp0w <joaopauloleireslopes.15@gmail.com>
2026-02-24 16:37:09 -0300

    responsividade no Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,29 @@
+import { useState } from "react"
+import MenuOutline from "./menu-outline"
+
 export default function Header(){
+
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+    
     return(
         <>
-            <header className="bg-header-bg text-header-text text-2xl flex justify-between items-center pr-3">
+            <header className={"bg-header-bg text-header-text text-2xl flex justify-between items-center pr-3 fixed top-0 w-full z-10"}>
                 <a className="flex items-center" href="./">
                     <img className="w-16 drop-shadow-xs drop-shadow-black" src="image.png" alt="" />
                     <span>Jspow</span>
                 </a>
-                <ul className="flex items-center gap-3 max-sm:hidden">
+                <button className="sm:hidden hover:cursor-pointer w-8 h-8" onClick={()=>{menuIsOpen?setMenuIsOpen(false):setMenuIsOpen(true)}} type="button">
+                    <MenuOutline menuIsOpen={menuIsOpen}/>
+                </button>
+                <ul className={"flex items-center gap-3 "+(menuIsOpen?'fixed top-16 bg-header-bg w-full h-screen flex-col items-end pr-3':'max-sm:hidden')}>
                     <li>
-                        <a href="./">Home</a>
+                        <a onClick={()=>{menuIsOpen?setMenuIsOpen(false):setMenuIsOpen(true)}} href="./">Home</a>
                     </li>
                     {/* <li className="bg-header-bg">
                         <a href="./">What i learned</a>
                     </li> */}
                     <li>
-                        <a href="./#/AboutMe">Sobre mim</a>
+                        <a onClick={()=>{menuIsOpen?setMenuIsOpen(false):setMenuIsOpen(true)}} href="./#/AboutMe">Sobre mim</a>
                     </li>
                     {/* <li className="bg-header-bg">
                         <a href="./">My projects</a>
