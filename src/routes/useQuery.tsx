fdde7b1fb9dd4a9b299d5c4bbe12f7942064c901
Jsp0w <joaopauloleireslopes.15@gmail.com>
2026-02-24 16:40:19 -0300

    removendo paginas que ainda não estão prontas

--- src/routes/useQuery.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/useQuery')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/useQuery"!</div>
-}
