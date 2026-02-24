09899acc4a9792763d2ca4c509ae4b44e29fbe79
Jsp0w <joaopauloleireslopes.15@gmail.com>
2026-02-24 16:42:09 -0300

    responsividade no abouteMe

--- src/routes/aboutMe.tsx
+++ src/routes/aboutMe.tsx
@@ -9,25 +9,25 @@ function RouteComponent() {
     <>
       <div>
         <h1 className='text-center text-8xl font-bold my-20'>Ola!</h1>
-        <div className='grid grid-cols-3'>
+        <div className='grid grid-cols-3 text-3xl max-sm:text-xl'>
           <img className='row-start-1 row-end-4 drop-shadow-md drop-shadow-black' src="perfil.jpg" alt="" />
-          <p className='col-start-2 col-end-4 pl-6 text-3xl font-extralight self-end'>Eu me chamo João Paulo, eu sou um tecnico de informatica e estudate de rede de computadores.</p>
-          <p className='col-start-2 col-end-4 pl-6 text-3xl font-extralight self-center'>Conheço as linguagens de programação Python, Java, Java Scrpit. Type Script, a Biblioteca React, e o Framework Angular alem da linguagem SQl para banco de dados.</p>
-          <p className='col-start-2 col-end-4 pl-6 text-3xl font-extralight self-start'>Atualmente cursando rede de computadores no Instituto Federal do Rio de Janeiro, enquanto busco uma oportinidade de ingressar no mercado de trabalho.</p>
+          <p className='col-start-2 col-end-4 pl-6 font-extralight self-end'>Eu me chamo João Paulo, eu sou um tecnico de informatica e estudate de rede de computadores.</p>
+          <p className='col-start-2 col-end-4 pl-6 font-extralight self-center'>Conheço as linguagens de programação Python, Java, Java Scrpit. Type Script, a Biblioteca React, e o Framework Angular alem da linguagem SQl para banco de dados.</p>
+          <p className='col-start-2 col-end-4 pl-6 font-extralight self-start'>Atualmente cursando rede de computadores no Instituto Federal do Rio de Janeiro, enquanto busco uma oportinidade de ingressar no mercado de trabalho.</p>
         </div>
         <div className='flex flex-col w-1/2 mx-auto my-10'>
           <h2 className='text-3xl text-emerald-700'>Estudos</h2>
           <p className='text-lg'>Eu começei meus estudos Instuto Federal do Rio de Janeiro fazendo o curso de tecnico de computadores integrado ao ensino médio. Atualmente estou no curso superor de rede de computadores, no mesmo instituto, que esta previsto terminar em dezembro de 2026</p>
           <h2 className='text-3xl text-emerald-700'>Contatos</h2>
           <ul className='text-lg list-disc pl-5'>
-            <li>E-mail:
-              <a href="mailto:joaopauloleiresloeps.15@gmali.com" target='_blank' >joaopauloleiresloeps.15@gmali.com</a>
+            <li>Email:
+              <a className='wrap-anywhere text-link-text' href="mailto:joaopauloleiresloeps.15@gmali.com" target='_blank' >joaopauloleiresloeps.15@gmali.com</a>
             </li>
             <li>Github:
-              <a href="https://github.com/Jsp0w" target='_blank'>Jsp0w</a>
+              <a className={'text-link-text'} href="https://github.com/Jsp0w" target='_blank'>Jsp0w</a>
             </li>
             <li>Linkedin:
-              <a href="./aboutMe">João Paulo Leires Lopes</a>
+              <a className={'text-link-text'} href="./aboutMe">João Paulo Leires Lopes</a>
             </li>
           </ul>
           {/* <a href="./aboutMe">Meu Curriculo em PDF</a> */}
